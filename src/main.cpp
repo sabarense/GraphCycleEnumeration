175c95706fc92e22f73a2ec0b78375880f17e8ac
Yan Sabarense <sabarensey@gmail.com>
2025-03-05 15:51:12 -0300

    [feat] add menu to choose DFS or Permutation

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "utils/caminhamento.cpp"
 #include "utils/grafo.cpp"
+#include "utils/permutacao.cpp"
 
 using namespace std;
 
 int main() {
     int numOfvertices;
+    int option; 
 
     cout << "Enter the number of vertices: ";
     cin >> numOfvertices;
@@ -15,14 +17,36 @@ int main() {
         cin >> numOfvertices;
     }
 
-    bool **adjMatrix = new bool*[numOfvertices];
+    bool **adjMatrix = new bool*[numOfvertices]();
 
     initAdjMatrix(adjMatrix, numOfvertices);
 
     printAdjMatrix(adjMatrix, numOfvertices);
 
-    cout << endl << "Enumerating all cycles of the graph..." << endl;
-    findCyclesDFS(adjMatrix, numOfvertices);
+    do {
+        cout << endl << "Choose an option:" << endl;
+        cout << "1. Find Cycles using DFS" << endl;
+        cout << "2. Find Cycles using Permutation" << endl;
+        cout << "0. Exit" << endl;
+        cin >> option;
+
+        switch (option) {
+            case 1:
+                cout << endl << "Enumerating all cycles of the graph..." << endl;
+                findCyclesDFS(adjMatrix, numOfvertices); 
+                break;
+            case 2:
+                cout << endl << "Enumerating all cycles of the graph..." << endl;
+                findCyclesPermutation(adjMatrix, numOfvertices); 
+                break;
+            case 0:
+                cout << "Exiting..." << endl;
+                break;
+            default:
+                cout << "Invalid option. Please try again." << endl;
+                break;
+        }
+    } while (option != 0);
 
     for (int i = 0; i < numOfvertices; i++) {
         delete[] adjMatrix[i];

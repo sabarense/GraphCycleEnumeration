204be800dab1adf8f36b5f36fb8ec9f3ad573067
Yan Sabarense <sabarensey@gmail.com>
2025-03-05 15:33:15 -0300

    [feat] add DFS methods & validations logic

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,29 @@
 using namespace std;
 
 int main() {
-	int numOfvertices;
+    int numOfvertices;
 
-	cout << "Enter the number of vertices: ";
-	cin >> numOfvertices;
+    cout << "Enter the number of vertices: ";
+    cin >> numOfvertices;
 
-	bool **adjMatrix = new bool*[numOfvertices];
+    while (numOfvertices <= 0) {
+        cout << "Invalid number of vertices! Please enter a positive number: ";
+        cin >> numOfvertices;
+    }
 
-	initAdjMatrix(adjMatrix, numOfvertices);
+    bool **adjMatrix = new bool*[numOfvertices];
 
-	printAdjMatrix(adjMatrix, numOfvertices);
+    initAdjMatrix(adjMatrix, numOfvertices);
+
+    printAdjMatrix(adjMatrix, numOfvertices);
+
+    cout << endl << "Enumerating all cycles of the graph..." << endl;
+    findCyclesDFS(adjMatrix, numOfvertices);
+
+    for (int i = 0; i < numOfvertices; i++) {
+        delete[] adjMatrix[i];
+    }
+    delete[] adjMatrix;
+
+    return 0;
 }
